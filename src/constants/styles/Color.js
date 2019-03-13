21025ef73911c3c164c50dad09848980eafd705c
Peter Laxalt <pete@laxaltandmciver.co>
2019-03-13 16:38:59 -0400

    add palette + typography

--- src/constants/styles/Color.js
+++ src/constants/styles/Color.js
@@ -4,12 +4,27 @@
 // This is the sitewide palette.
 
 export const Color = {
-  Primary: '#ffffff',
-  Secondary: '#FAFAFA',
-  Background: '#ffffff',
-  White: `#ffffff`,
+  // Core Colors
+  Primary: '#40DA33',
+  Secondary: '#5D6376',
+  Background: '#F2F3F3',
+
+  // Grayscale Palette
   Black: `#000000`,
+  Nightsky: `#262222`,
+  Gunmetal: '#5D6376',
+  Slate: '#AEB9C4',
   Gray: `#CCD0D6`,
-  Warning: `Blue`,
-  Success: `PaleGreen`,
+  Snow: `#F2F3F3`,
+  White: `#ffffff`,
+
+  // Main Palette
+  Leaf: '#40DA33',
+  Tahoe: '#2984FD',
+  Sunset: '#FC6376',
+  Dank: '#F8E71C',
+
+  // Feedback Palette
+  Warning: `#FC6376`,
+  Success: `#40DA33`,
 };
