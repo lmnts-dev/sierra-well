a4286797f706ff4f0ce39019d3f062a13dbf1d12
Peter Laxalt <pete@laxaltandmciver.co>
2019-05-22 12:37:10 -0400

    build test

--- public/component---src-pages-404-js-a482ef0ce9275db5a2b2.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{237:function(n,e,t){"use strict";t.r(e);var a=t(0),u=t.n(a),o=t(275);e.default=function(){return u.a.createElement(o.a,null,u.a.createElement("div",null,"Not found."))}}}]);
-//# sourceMappingURL=component---src-pages-404-js-a482ef0ce9275db5a2b2.js.map
